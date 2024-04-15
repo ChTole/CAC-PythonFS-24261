8f110c3f7f87c1bbc9f1d5beeeb39ce3e60dbd45
ChTole <ch.ariel.tole@gmail.com>
2024-04-15 11:41:27 -0300

    Agrego clases de Git e intro a JS

---
+++ Clase13/js/intro.js
@@ -0,0 +1,18 @@
+// Tipos de datos
+
+// Primitivos
+// Boolean, Number, BigInt, String, Symbol y undefined
+
+// Complejos
+// Array - arreglo (vector) --> Python: Listas
+// Object - objeto (atributos y métodos) --> Python: Diccionarios
+// null
+
+typeof 5 // 'number'
+typeof 3.14 // 'number'
+typeof 5n // 'bigint'
+typeof true // 'boolean'
+typeof True // 'undefined' --> 
+
+// Debilmente tipado
+typeof ("123" + 1) // 'string'
