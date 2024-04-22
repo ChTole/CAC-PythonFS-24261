68a89cd9c258a5ace0622a0114e1ba3471ddec64
ChTole <ch.ariel.tole@gmail.com>
2024-04-22 09:22:16 -0300

    Agrego temario clase 15 - JS Operadores y estructuras

---
+++ Clase15/js/main.js
@@ -0,0 +1,24 @@
+// NOMBRES DE VARIABLES (ver palabras reservadas)
+// - Pueden comenzar con una letra, un guión bajo ( _ ) o un signo de dólar ( $ ).
+// - Los siguientes caracteres también pueden ser dígitos ( 0 - 9 ), nunca al principio. 
+// - JavaScript distingue entre mayúsculas y minúsculas (es case-sensitive).
+// - Cuando contienen más de una palabra se utiliza la notación camelCase.
+// - Deben ser referenciales de su contenido.
+
+// OPERADORES
+// Aritméticos: + , - , * , / , ** , %
+
+// De comparación: < , > , <= , >= , == , !=
+
+// Lógicos: && (conjunción ó «y»), || (disyunción u «ó») y ! (negación -unario-)
+
+// CUADROS DE DIÁLOGO (alert, prompt, confirm)
+// No son de uso habitual, pero en éste punto nos sirven para interactuar con nuestra página, sitio o aplicación.
+// Estos mensajes automatizados tienen dos inconvenientes:
+// No hay una forma estándar de cambiar su aspecto con CSS.
+// Dependen de la configuración regional del navegador, lo que significa que puedes tener una página en un idioma pero un mensaje de error en otro idioma.
+
+// CONTROL DE FLUJO
+// Estructuras condicionales (if - if...else - if...else if...else - switch...case)
+// Estructuras repetitivas indeterminadas (while - do...while)
+// Estructuras repetitivas determinadas (for (desde,hasta,paso) - for...of - for...in)
